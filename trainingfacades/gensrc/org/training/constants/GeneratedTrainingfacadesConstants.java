f0b5b0a565d8ef29589e6f701090e2abd97d8e77
Peter Trestyanszki <peter_trestyanszki@epam.com>
2017-12-31 14:55:17 -0800

    Add new extension for assignments

---
+++ trainingfacades/gensrc/org/training/constants/GeneratedTrainingfacadesConstants.java
@@ -0,0 +1,24 @@
+/*
+ * ----------------------------------------------------------------
+ * --- WARNING: THIS FILE IS GENERATED AND WILL BE OVERWRITTEN! ---
+ * --- Generated at Dec 31, 2017 2:26:44 PM                     ---
+ * ----------------------------------------------------------------
+ */
+package org.training.constants;
+
+/**
+ * @deprecated use constants in Model classes instead
+ */
+@Deprecated
+@SuppressWarnings({"unused","cast","PMD"})
+public class GeneratedTrainingfacadesConstants
+{
+	public static final String EXTENSIONNAME = "trainingfacades";
+	
+	protected GeneratedTrainingfacadesConstants()
+	{
+		// private constructor
+	}
+	
+	
+}
